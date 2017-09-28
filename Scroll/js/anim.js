8c81d8bafbf54daaf8ae4d668e70e28f8598c6f1
Jeyem <jmlf1223@gmail.com>
2017-09-29 03:27:15 +0800

    improved menu style

--- Scroll/js/anim.js
+++ Scroll/js/anim.js
@@ -1,92 +1,143 @@
 
-var canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var ctx = canvas.getContext("2d");
 
-var TAU = 2 * Math.PI;
+var circles = [],
+    canvas = document.getElementById("canvas1"),
+    context = canvas.getContext("2d"),
+    
+    // SETTINGS 
+    opacity = 0.6,                                      // the opacity of the circles 0 to 1
+    colors = ['rgba(34, 49, 63,' + opacity + ')',       // an array of rgb colors for the circles
+              'rgba(189, 195, 199,' + opacity + ')',
+              'rgba(241, 196, 15,' + opacity + ')',
+              'rgba(231, 76, 60,' + opacity + ')',
+              'rgba(231, 76, 60,' + opacity + ')'
+             ],
+    minSize = 1,                                        // the minimum size of the circles in px
+    maxSize = 10,                                       // the maximum size of the circles in px
+    numCircles = 300,                                   // the number of circles
+    minSpeed = -2,                                     // the minimum speed, recommended: -maxspeed
+    maxSpeed = 2,                                    // the maximum speed of the circles
+    expandState = true;                                      // the direction of expansion
 
-times = [];
-function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  update();
-  draw();
-  requestAnimationFrame(loop);
-}
-
-function Ball (startX, startY, startVelX, startVelY) {
-  this.x = startX || Math.random() * canvas.width;
-  this.y = startY || Math.random() * canvas.height;
-  this.vel = {
-    x: startVelX || Math.random() * 2 - 1,
-    y: startVelY || Math.random() * 2 - 1
-  };
-  this.update = function(canvas) {
-    if (this.x > canvas.width + 50 || this.x < -50) {
-      this.vel.x = -this.vel.x;
+function buildArray() {
+    'use strict';
+    
+    for (var i =0; i < numCircles ; i++){
+        var color = Math.floor(Math.random() * (colors.length - 1 + 1)) + 1,
+            left = Math.floor(Math.random() * (canvas.width - 0 + 1)) + 0,
+            top = Math.floor(Math.random() * (canvas.height - 0 + 1)) + 0,
+            size = Math.floor(Math.random() * (maxSize - minSize + 1)) + minSize,
+            leftSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10,
+            topSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10,
+            expandState = expandState;
+           
+            while(leftSpeed == 0 || topSpeed == 0){
+                leftSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10,
+                topSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10;
+            }
+        var circle = {color:color, left:left, top:top, size:size, leftSpeed:leftSpeed, topSpeed:topSpeed, expandState:expandState };
+        circles.push(circle);
     }
-    if (this.y > canvas.height + 50 || this.y < -50) {
-      this.vel.y = -this.vel.y;
-    }
-    this.x += this.vel.x;
-    this.y += this.vel.y;
-  };
-  this.draw = function(ctx, can) {
-    ctx.beginPath();
-    ctx.globalAlpha = .4;
-    ctx.fillStyle = 'red';
-    ctx.arc((0.5 + this.x) | 0, (0.5 + this.y) | 0, 3, 0, TAU, false);
-    ctx.fill();
-  }
 }
 
-var balls = [];
-for (var i = 0; i < canvas.width * canvas.height / (65*65); i++) {
-  balls.push(new Ball(Math.random() * canvas.width, Math.random() * canvas.height));
-}
-
-var lastTime = Date.now();
-function update() {
-  var diff = Date.now() - lastTime;
-  for (var frame = 0; frame * 16.6667 < diff; frame++) {
-    for (var index = 0; index < balls.length; index++) {
-      balls[index].update(canvas);
-    }
-  }
-  lastTime = Date.now();
-}
-var mouseX = -1e9, mouseY = -1e9;
-document.addEventListener('mousemove', function(event) {
-  mouseX = event.clientX;
-  mouseY = event.clientY;
-});
-
-function distMouse(ball) {
-  return Math.hypot(ball.x - mouseX, ball.y - mouseY);
-}
-
-function draw() {
-  ctx.globalAlpha=1;
-  ctx.fillStyle = 'transparent';
-  ctx.fillRect(0,0,canvas.width, canvas.height);
-  for (var index = 0; index < balls.length; index++) {
-    var ball = balls[index];
-    ball.draw(ctx, canvas);
-    ctx.beginPath();
-    for (var index2 = balls.length - 1; index2 > index; index2 += -1) {
-      var ball2 = balls[index2];
-      var dist = Math.hypot(ball.x - ball2.x, ball.y - ball2.y);
-        if (dist < 100) {
-          ctx.strokeStyle = "#448fda";
-          ctx.globalAlpha = 1 - (dist > 100 ? .8 : dist / 150);
-          ctx.lineWidth = "2px";
-          ctx.moveTo((0.5 + ball.x) | 0, (0.5 + ball.y) | 0);
-          ctx.lineTo((0.5 + ball2.x) | 0, (0.5 + ball2.y) | 0);
+function build(){
+    'use strict';
+    
+    for(var h = 0; h < circles.length; h++){
+        var curCircle = circles[h];
+        context.fillStyle = colors[curCircle.color-1];
+        context.beginPath();
+        if(curCircle.left > canvas.width+curCircle.size){
+            curCircle.left = 0-curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+        }else if(curCircle.left < 0-curCircle.size){
+            curCircle.left = canvas.width+curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+        }else{
+            curCircle.left = curCircle.left+curCircle.leftSpeed;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false); 
         }
+        
+        if(curCircle.top > canvas.height+curCircle.size){
+            curCircle.top = 0-curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+
+        }else if(curCircle.top < 0-curCircle.size){
+            curCircle.top = canvas.height+curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+        }else{
+            curCircle.top = curCircle.top+curCircle.topSpeed;
+            if(curCircle.size != maxSize && curCircle.size != minSize && curCircle.expandState == false){
+              curCircle.size = curCircle.size-0.1;
+            }
+            else if(curCircle.size != maxSize && curCircle.size != minSize && curCircle.expandState == true){
+              curCircle.size = curCircle.size+0.1;
+            }
+            else if(curCircle.size == maxSize && curCircle.expandState == true){
+              curCircle.expandState = false;
+              curCircle.size = curCircle.size-0.1;
+            }
+            else if(curCircle.size == minSize && curCircle.expandState == false){
+              curCircle.expandState = true;
+              curCircle.size = curCircle.size+0.1;
+            }
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false); 
+        }
+        
+        context.closePath();
+        context.fill();
+        context.ellipse;
     }
-    ctx.stroke();
-  }
 }
 
-// Start
-loop();
+
+var xVal = 0;
+
+window.requestAnimFrame = (function (callback) {
+    'use strict';
+    return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+        window.setTimeout(callback, 1000/60);
+    };
+})();
+
+function animate() {
+    'use strict';
+    var canvas = document.getElementById("canvas1"),
+        context = canvas.getContext("2d");
+
+    // clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    // draw the next frame
+    xVal++;
+    build();
+
+    //console.log("Prep: animate ==> requestAnimFrame");
+    // request a new frame
+    requestAnimFrame(function () {
+        animate();
+    });
+}
+window.onload = function () {
+    'use strict';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    buildArray();
+    animate();
+};
+
+
+window.onresize = function () {
+    'use strict';
+    console.log("resize");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    //buildArray();
+    animate();
+};
